8a87fd1c887b25e14cc3df1ebdd8eff35385fde0
Astrohusky204 <120604486+Astrohusky204@users.noreply.github.com>
2022-12-16 21:21:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,7 @@ var character =
 var block = document.getElementById("block");
 function jump(){
     character.classList.add("animate");
+    setTimeout(funtion(){
+        character.classList.remove("animate");
+     },500);
 }
